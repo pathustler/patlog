7928cc3549abd113b0537e3aeafb27f3ce4caa69
pathustler <patlowharry@gmail.com>
2025-06-17 00:17:05 +1000

    change hostname

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "img.freepik.com/**",
+        hostname: "patlog.vercel.app",
+        pathname: "/**", // Allows all paths under the hostname
       },
     ],
   },
-  // if used turbopack
-  // transpilePackages: ["next-mdx-remote"],
 };
 
 export default nextConfig;
